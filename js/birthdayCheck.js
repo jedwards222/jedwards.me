05b6c384e280abeec90d06614ead3c2f8b4363b2
jedwards222 <jedwards@usagb.net>
2017-01-18 23:33:39 -0800

    moved javascript to separate files

---
+++ js/birthdayCheck.js
@@ -0,0 +1,15 @@
+<!-- Begin
+var now = new Date();
+if (now.getMonth() === 1 && now.getDate() === 10) {
+  document.write("Today is my Birthday!");
+}
+else {
+  document.write("Today is not my birthday :( ");
+  document.write("Try again tomorrow!");
+}
+// Styling for the Birthday message
+document.getElementById("birthday").style.font = "20px avenir,serif";
+document.getElementById("birthday").style.width = "250px";
+document.getElementById("birthday").style.margin = "20px auto";
+document.getElementById("birthday").style.color = "navy";
+//  End -->
