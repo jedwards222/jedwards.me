993de5699dcfce8ee15ef554ee9883fcd36f11b6
jedwards222 <jedwards@usagb.net>
2017-01-20 13:16:25 -0800

    added birthday mail link

--- js/birthdayCheck.js
+++ js/birthdayCheck.js
@@ -4,18 +4,25 @@ var monthsAway = (now.getMonth() - 1);
 var daysAway = (now.getDate() - 10);
 
 if (monthsAway === 0 && daysAway === 0) {
-  document.write("<p>Today is my Birthday!</p>");
+  var birthday = document.createElement('a');
+  var linkText = document.createTextNode('Send me a birthday message!');
+  birthday.appendChild(linkText);
+  birthday.title = 'Birthday Message';
+  birthday.href = 'mailto:james.g.edwards.18@dartmouth.edu';
+  birthday.id = 'birthdayMail';
+  document.write('<p>Today is my Birthday!</p>');
+  document.body.append(birthday);
 }
 else {
   var message;
   if (monthsAway === 0 && daysAway > 0) {
-    message = "Try again tomorrow!";
+    message = 'Try again tomorrow!';
   } else if (monthsAway > 0 && monthsAway < 6) {
-    message = "Try again next year!";
+    message = 'Try again next year!';
   } else if ( monthsAway >= 6) {
-    message = "Try again in a few months!";
-  } else message = "Try again soon!";
+    message = 'Try again in a few months!';
+  } else message = 'Try again soon!';
 
-  document.write("<p>Today is not my birthday :(</p>");
+  document.write('<p>Today is not my birthday :(</p>');
   document.write(`<p>${message}</p>`);
 }
