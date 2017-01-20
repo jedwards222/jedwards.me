871c1bee1d12fe71031b7934fe5331bab012a85f
jedwards222 <jedwards@usagb.net>
2017-01-19 22:59:12 -0800

    restructured stylesheet and added alt descriptions

--- js/birthdayCheck.js
+++ js/birthdayCheck.js
@@ -4,12 +4,13 @@ if (now.getMonth() === 1 && now.getDate() === 10) {
   document.write("Today is my Birthday!");
 }
 else {
-  document.write("Today is not my birthday :( ");
-  document.write("Try again tomorrow!");
+  document.write("<p>Today is not my birthday :(</p>");
+  document.write("<p>Try again tomorrow!</p>");
 }
 // Styling for the Birthday message
 document.getElementById("birthday").style.font = "20px avenir,serif";
 document.getElementById("birthday").style.width = "250px";
-document.getElementById("birthday").style.margin = "20px auto";
+document.getElementById("birthday").style.margin = "0 auto";
+document.getElementById("birthday").style.padding = "0 auto";
 document.getElementById("birthday").style.color = "navy";
 //  End -->
